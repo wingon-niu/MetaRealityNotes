d2552b0fac18628d568923e35a60ca094c0bc58c
niuyg <y-g.niu@163.com>
2021-11-22 09:56:49 +0000

    edit contract

---
+++ contracts/eos/worldwelfare/worldwelfare.cpp
@@ -0,0 +1,35 @@
+#include "worldwelfare.hpp"
+
+// 接收用户转账，并调用 metarealnote 合约，为用户新增转账信息
+[[eosio::on_notify("eosio.token::transfer")]]
+void worldwelfare::deposit(name from, name to, eosio::asset quantity, std::string memo)
+{
+    if (from == _self) {
+        return;
+    }
+    if (to != _self) {
+        return;
+    }
+    if (!quantity.symbol.is_valid()) {
+        return;
+    }
+    if (!(quantity.symbol == MAIN_SYMBOL)) {
+        return;
+    }
+    if (!quantity.is_valid()) {
+        return;
+    }
+    if (!quantity.is_amount_within_range()) {
+        return;
+    }
+    if (!(quantity.amount > 0)) {
+        return;
+    }
+
+    action{
+        permission_level{get_self(), "active"_n},
+        "metarealnote"_n,
+        "addaccount"_n,
+        std::make_tuple(from, quantity)
+    }.send();
+}
---
+++ contracts/eos/worldwelfare/worldwelfare.hpp
@@ -0,0 +1,32 @@
+#include <eosio/eosio.hpp>
+#include <eosio/time.hpp>
+#include <eosio/system.hpp>
+#include <eosio/asset.hpp>
+#include <eosio/contract.hpp>
+#include <eosio/dispatcher.hpp>
+#include <eosio/print.hpp>
+#include <utility>
+#include <vector>
+#include <string>
+
+#define  MAIN_SYMBOL     symbol(symbol_code("SYS"), 4)
+#define  ZERO_FEE        asset((int64_t)0, MAIN_SYMBOL)   // 0 EOS
+
+using namespace eosio;
+using std::string;
+
+// worldwelfare
+// 公益基金
+
+CONTRACT worldwelfare : public eosio::contract {
+
+public:
+
+    using contract::contract;
+
+    worldwelfare(name self, name first_receiver, datastream<const char*> ds) : contract(self, first_receiver, ds){};
+
+    // 接收用户转账，并调用 metarealnote 合约，为用户新增转账信息
+    [[eosio::on_notify("eosio.token::transfer")]]
+    void deposit(name from, name to, eosio::asset quantity, std::string memo);
+};
