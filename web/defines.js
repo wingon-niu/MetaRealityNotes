41f7d718e47006e03eb93f9cacbcd6373d20bd72
niuyg <y-g.niu@163.com>
2021-11-30 09:32:45 +0000

    edit web

---
+++ web/defines.js
@@ -0,0 +1,48 @@
+
+const runmode               = "dev";                    // prod or dev
+
+const worldwelfare_contract = "worldwelfare";
+const metarealnote_contract = "metarealnote";
+const pubwelfmedal_contract = "pubwelfmedal";
+
+const my_app_name_eos       = "metarealnote";
+
+var eos_chain_id            = null;
+var eos_network             = null;
+
+if (runmode === "prod") { // 生产环境
+	eos_chain_id     = "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191";
+
+	eos_network = {
+		blockchain: 'eos',
+		protocol: 'https',
+		host: 'api-kylin.eosasia.one',
+		port: 443,
+		chainId: eos_chain_id
+	}
+} else { // 开发测试环境
+	eos_chain_id     = "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191";
+
+	eos_network = {
+		blockchain: 'eos',
+		protocol: 'https',
+		host: 'api-kylin.eosasia.one',
+		port: 443,
+		chainId: eos_chain_id
+	}
+}
+
+const eos_endpoint     = eos_network.protocol     + '://' + eos_network.host     + ':' + eos_network.port;
+
+var current_block_chain   = "eos";
+var current_my_app_name   = my_app_name_eos;
+var current_network       = eos_network;
+var current_endpoint      = eos_endpoint;
+var current_user_account  = "";
+var current_note_category = "real";      // "real" or "dream"
+var doc_scroll_top        = 0;
+var scatter               = null;
+
+const name_max_len   = 36;
+const detail_max_len = 512;
+
---
+++ web/translate.js
@@ -0,0 +1,58 @@
+
+function set_lang_cookie(lang_type)
+{
+	var exdate = new Date();
+	exdate.setDate(exdate.getDate() + 30000);
+	document.cookie = "i18n_lang=" + escape(lang_type) + "^;expires=" + exdate.toGMTString() + ";path=/";
+}
+
+function get_cookie(c_name)
+{
+	if (document.cookie && document.cookie != '') {
+		c_start = document.cookie.indexOf(c_name + "=");
+		if (c_start != -1) { 
+			c_start = c_start + c_name.length + 1;
+			c_end = document.cookie.indexOf("^", c_start);
+			if (c_end == -1) c_end = document.cookie.length;
+			return unescape(document.cookie.substring(c_start, c_end));
+		}
+	}
+	return "";
+}
+
+function change_lang(lang_type)
+{
+	set_lang_cookie(lang_type);
+
+	jQuery.i18n.properties({
+		name : "strings",
+		path : "/",
+		mode : 'map',
+		language : lang_type,
+		cache : false,
+		encoding : 'UTF-8',
+		async : false,
+		callback : function() {
+			$("#header_title").html($.i18n.prop('header_title'));
+			$("#login").html($.i18n.prop('login'));
+			$("#user_must_read").html($.i18n.prop('user_must_read'));
+			$("#logoff").html($.i18n.prop('logoff'));
+			$("#tab_real").html($.i18n.prop('tab_real'));
+			$("#tab_dream").html($.i18n.prop('tab_dream'));
+			$("#write_an_article").html($.i18n.prop('write_an_article'));
+			$("#user_info").html($.i18n.prop('user_info'));
+
+			//$(".quantity_example").html($.i18n.prop('quantity_example'));
+			//$(".my_cancel").html($.i18n.prop('my_cancel'));
+			//$(".my_confirm").html($.i18n.prop('my_confirm'));
+			//$(".my_close").html($.i18n.prop('my_close'));
+			//$("#target_private_album_id").html($.i18n.prop('target_private_album_id'));
+			//$("#target_public_album_id").html($.i18n.prop('target_public_album_id'));
+			//if (current_album_type == "public") {
+			//	$("#current_album_type").html($("#tab_public_album").html());
+			//} else {
+			//	$("#current_album_type").html($("#tab_private_album").html());
+			//}
+		}
+	});
+}
