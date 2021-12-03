fa92816d7d4f678cba2248f005bcd2a99e17792a
niuyg <y-g.niu@163.com>
2021-12-03 09:57:07 +0000

    edit web

--- web/home.js
+++ web/home.js
@@ -143,6 +143,29 @@ function check_post_article()
 function view_times_of_txn_article()
 {
 	if (check_post_article() === false) return;
+
+	let my_type               = Number($("input[name='radio11']:checked").val());
+	let my_storage_location   = Number($("input[name='radio12']:checked").val());
+	let my_content            = $("#content_of_article").val();
+
+	let times_of_txn = 1;     // 需要的交易次数
+	let per_trn_len  = 10;    // 每个交易memo存放的数据长度，不同的链设置不同的值
+
+	if (my_storage_location === 1) {             // 内容数据存储在 EOS 链
+		if (my_type === 2) {                     // 长文
+			times_of_txn += 1;                   // 长文的标题单独保存在一个交易的memo里
+		}
+		per_trn_len = eos_per_trn_len;
+	} else {
+		return;
+	}
+
+	let my_len = my_content.length;
+	let my_num = Math.ceil(my_len / per_trn_len);  // 按照长度将内容分割为多个部分，计算数量，向上取整
+	times_of_txn += my_num;
+
+	if (get_cookie('i18n_lang') === "zh") alert("需要发送的交易次数总共为：" + times_of_txn + "。请确保您的帐户有足够余额以及足够的CPU/NET资源。");
+	else                                  alert("The total number of transactions to be sent is: " + times_of_txn + ". Please ensure that your account has sufficient balance and sufficient CPU/NET resources");
 }
 
 function post_article()
