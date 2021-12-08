edb83fc070f3a15bb17e33723481937d566dfe65
niuyg <y-g.niu@163.com>
2021-12-08 10:29:48 +0000

    edit web

--- web/pagecontrol.js
+++ web/pagecontrol.js
@@ -97,31 +97,16 @@ function go_back()
 		if (articles_array.length > 1) { // 还有打开的文章
 			show_article_content_div(articles_array[articles_array.length - 1]);
 		} else {                         // 没有打开的文章了
-			if        () {
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
-			} else if () {
-			}
+			current_article_id = 0;
+			$("#article_content_div").hide();
+			if      (current_page === "home")                       { $("#all_tabs").show();                   }
+			else if (current_page === "my_articles")                { $("#my_articles_div").show();            }
+			else if (current_page === "my_replies")                 { $("#my_replies_div").show();             }
+			else if (current_page === "articles_of_user_i_follow")  {                                          }
+			else if (current_page === "articles_of_user_follow_me") {                                          }
+			else                                                    { alert("go_back unknown error.");         }
+			window.scrollTo(0, doc_scroll_top);
 		}
 	} else {                             // 不在打开的文章中
-	}
-
-	if (current_note_category != "" && current_article_id != 0) {
-		current_article_id = 0;
-		$("#article_content_div").hide();
-		$("#all_tabs").show();
-		window.scrollTo(0, doc_scroll_top);
 	}
 }
