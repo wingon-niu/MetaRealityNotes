aa34ce523d86b44e8042ab3be34af13dce12e921
niuyg <y-g.niu@163.com>
2021-12-10 14:01:17 +0000

    edit web

--- web/query.js
+++ web/query.js
@@ -246,47 +246,49 @@ function show_article_content_div(article_id)
 					$("#content_page_of_article_" + resp.rows[i].article_id).html(my_escapeHTML(content));
 				}
 				//
+				lower_bd  = new BigNumber(article_id);
+				lower_bd  = lower_bd.multipliedBy(4294967296); // 4294967296 = 2的32次方，相当于左移32位。
+				lower_bd  = lower_bd.multipliedBy(4294967296); // 4294967296 = 2的32次方，相当于左移32位。
+
+				upper_bd  = new BigNumber(article_id + 1);
+				upper_bd  = upper_bd.multipliedBy(4294967296); // 4294967296 = 2的32次方，相当于左移32位。
+				upper_bd  = upper_bd.multipliedBy(4294967296); // 4294967296 = 2的32次方，相当于左移32位。
+
 				resp = await rpc.get_table_rows({
 					json:  true,
 					code:  metarealnote_contract,
 					scope: metarealnote_contract,
-					table: 'articles',
-					index_position: index_pos,
-					key_type: 'i64',
+					table: 'replies',
+					index_position: 2,
+					key_type: 'i128',
 					lower_bound: lower_bd,
 					upper_bound: upper_bd,
-					limit: 1,
+					limit: 100,
 					reverse: false,
 					show_payer: false
 				});
-				let articles = '';
+				let replies = '';
 				let i = 0;
 				let len = resp.rows.length;
 				if (len === 0) {
-					if (get_cookie('i18n_lang') === "zh") articles = '<p>文章不存在或者已经被删除。</p>';
-					else                                  articles = '<p>The article does not exist or has been deleted.</p>';
+					replies = '&nbsp;';
 				}
 				for (i = 0; i < len; i++) {
-					let f = '<a href="##" onclick="alert(\'' + $("#head_hash").html() + storage_locations[resp.rows[i].storage_location] + '{' + resp.rows[i].article_hash + '}\');">id' + resp.rows[i].article_id + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-					if (resp.rows[i].forward_article_id > 0) {
-						f = f + '<span>' + $("#forward_article").html() + '</span>&nbsp;<a href="##" onclick="show_article_content_div(' + resp.rows[i].forward_article_id + ');">id' + resp.rows[i].forward_article_id + '</a>';
-					}
-					articles = articles + '<div><table width="100%" border="0">';
-					articles = articles + '<tr>' + '<td rowspan="3" width="64" align="center" valign="top"><span class="am-icon-user"></span></td>' + '<td>' + resp.rows[i].user + '&nbsp;&nbsp;' + timestamp_trans_full(resp.rows[i].post_time) + '</td>' + '</tr>';
-					articles = articles + '<tr>' + '<td>' + f + '<pre id="content_page_of_article_' + resp.rows[i].article_id + '">&nbsp;</pre></td>' + '</tr>';
-					articles = articles + '<tr>' + '<td align="right"><a href="##" onclick="forward_an_article(' + resp.rows[i].article_id + ');">' + $("#forward").html() + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="##" onclick="reply_an_article(' + resp.rows[i].article_id + ');">' + $("#reply").html() + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="am-icon-share"></span>&nbsp;' + resp.rows[i].forwarded_times + '&nbsp;&nbsp;&nbsp;&nbsp;<span class="am-icon-comment"></span>&nbsp;' + resp.rows[i].replied_times + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>' + '</tr>';
-					articles = articles + '</table></div><hr />';
+					let f = '<a href="##" onclick="alert(\'' + $("#head_hash").html() + storage_locations[resp.rows[i].storage_location] + '{' + resp.rows[i].reply_hash + '}\');">id' + resp.rows[i].reply_id + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+					replies = replies + '<div><table width="100%" border="0">';
+					replies = replies + '<tr>' + '<td rowspan="3" width="64" align="center" valign="top"><span class="am-icon-user"></span></td>' + '<td>' + resp.rows[i].user + '&nbsp;&nbsp;' + timestamp_trans_full(resp.rows[i].post_time) + '</td>' + '</tr>';
+					replies = replies + '<tr>' + '<td>' + f + '<pre id="content_of_reply_' + resp.rows[i].reply_id + '">&nbsp;</pre></td>' + '</tr>';
+					replies = replies + '<tr>' + '<td align="right"><span class="am-icon-comment"></span>&nbsp;' + resp.rows[i].replied_times + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>' + '</tr>';
+					replies = replies + '</table></div><hr />';
 				}
-				$("#article_content_info_div").html(articles);
-				$("#my_modal_loading").modal('close');
+				$("#article_replies_div").html(replies);
 				for (i = 0; i < len; i++) {
 					let memo        = '';
 					let next_hash   = '';
 					let content     = '';
 					let transaction = null;
 					if (resp.rows[i].storage_location === 1) {                                        // 数据存储在 EOS 链上
-						next_hash      = resp.rows[i].article_hash;
-						let first_loop = true;
+						next_hash      = resp.rows[i].reply_hash;
 						do {
 							transaction = await rpc.history_get_transaction(next_hash);
 							memo = transaction.trx.trx.actions[0].data.memo;
@@ -297,15 +299,11 @@ function show_article_content_div(article_id)
 								next_hash = '';
 							}
 							content = content + memo.slice(memo.indexOf('}') + 1, memo.length);
-							if (resp.rows[i].type === 2 && first_loop) {                              // 长文
-								content = '        ' + content + '\n';
-							}
-							first_loop = false;
 						} while (next_hash != '');
 					}
 					else {      // 数据存储在其他链上
 					}
-					$("#content_page_of_article_" + resp.rows[i].article_id).html(my_escapeHTML(content));
+					$("#content_of_reply_" + resp.rows[i].reply_id).html(my_escapeHTML(content));
 				}
 				//
 				hide_all_pages();
