1dec2f17f58059a04b3b9f27dd88bef57c5994db
niuyg <y-g.niu@163.com>
2021-12-10 04:32:34 +0000

    edit web

--- web/postreply.js
+++ web/postreply.js
@@ -5,4 +5,16 @@ function reply_an_article(article_id)
 		alert($("#please_login").html());
 		return;
 	}
+	$("#target_article_id").val(article_id.toString());
+	$("#target_reply_id").val("0");
+	write_a_reply_show_modal();
+}
+
+function write_a_reply_show_modal()
+{
+	$('#div_write_a_reply').modal({
+		relatedTarget: this,
+		onCancel: function() {},
+		onConfirm: function() {}
+	});
 }
