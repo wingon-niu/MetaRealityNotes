3f6a8e81bf6930b6a59ce56ada395756681fe262
niuyg <y-g.niu@163.com>
2021-12-16 04:22:37 +0000

    edit web

--- web/profile.js
+++ web/profile.js
@@ -1,8 +1,16 @@
 
 function create_edit_profile()
 {
+	if(current_user_account === "") {
+		alert($("#please_login").html());
+		return;
+	}
 }
 
 function delete_profile()
 {
+	if(current_user_account === "") {
+		alert($("#please_login").html());
+		return;
+	}
 }
