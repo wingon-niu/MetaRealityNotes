60d5879b167c2dea58ea2f9e09b3d8b568a11b43
niuyg <y-g.niu@163.com>
2021-12-22 10:00:49 +0000

    edit web

--- web/query.js
+++ web/query.js
@@ -69,7 +69,7 @@ function get_articles(index_position, key_type, lower_bound, upper_bound)
 			}
 			// 下一页
 			if (resp.more === true) {
-				articles = articles + '<br /><table width="100%" border="0"><tr><td align="center"><a href="##" onclick="get_articles(' + index_position + ', \'' + key_type + '\', \'' + resp.next_key + '\', \'' + upper_bound + '\');">' + $("#next_page").html() + '</a></td></tr></table>';
+				articles = articles + '<table width="100%" border="0"><tr><td align="center"><a href="##" onclick="get_articles(' + index_position + ', \'' + key_type + '\', \'' + resp.next_key + '\', \'' + upper_bound + '\');">' + $("#next_page").html() + '</a></td></tr></table>';
 			}
 			// 以下按照当前页面将所有文章的基本信息赋值过去
 			if (current_page === "home") {
@@ -115,6 +115,7 @@ function get_articles(index_position, key_type, lower_bound, upper_bound)
 				$(".preview_of_article_" + resp.rows[i].article_id).html(my_escapeHTML(content));
 			}
 			$("#my_modal_loading").modal('close');
+			window.scrollTo(0, 0);
 		} catch (e) {
 			$("#my_modal_loading").modal('close');
 			alert(e);
