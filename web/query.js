786a6ea9e17fef5a6bfacb1a4bb512802a6e8385
niuyg <y-g.niu@163.com>
2021-12-23 07:40:31 +0000

    edit web

--- web/query.js
+++ web/query.js
@@ -263,95 +263,6 @@ function show_article_content_div(article_id)
 
 				get_replies(2, 'i128', lower_bd.toFixed(), upper_bd.toFixed());
 
-				resp = await rpc.get_table_rows({
-					json:  true,
-					code:  metarealnote_contract,
-					scope: metarealnote_contract,
-					table: 'replies',
-					index_position: 2,
-					key_type: 'i128',
-					lower_bound: lower_bd.toFixed(),
-					upper_bound: upper_bd.toFixed(),
-					limit: 100,
-					reverse: false,
-					show_payer: false
-				});
-				let replies = '';
-				i   = 0;
-				len = resp.rows.length;
-				if (len === 0) {
-					replies = '&nbsp;';
-				}
-				// 以下生成所有回复的基本信息
-				for (i = 0; i < len; i++) {
-					let f = '<a href="##" onclick="alert(\'' + $("#head_hash").html() + storage_locations[resp.rows[i].storage_location] + '{' + resp.rows[i].reply_hash + '}\');">id' + resp.rows[i].reply_id + '</a>&nbsp;&nbsp;';
-					if (resp.rows[i].target_reply_id > 0) {
-						f = f + $("#reply_to").html() + '&nbsp;<span class="user_of_reply_' + resp.rows[i].target_reply_id + '">&nbsp;</span>&nbsp;id' + resp.rows[i].target_reply_id;
-					}
-					replies = replies + '<div><table width="100%" border="0">';
-					replies = replies + '<tr>' + '<td rowspan="3" width="30" align="center" valign="top">&nbsp;</td>' + '<td rowspan="3" width="64" align="center" valign="top"><span class="am-icon-user"></span></td>' + '<td>' + resp.rows[i].user + '&nbsp;&nbsp;' + timestamp_trans_full(resp.rows[i].post_time) + '</td>' + '</tr>';
-					replies = replies + '<tr>' + '<td>' + f + '<pre class="content_of_reply_' + resp.rows[i].reply_id + '">&nbsp;</pre></td>' + '</tr>';
-					replies = replies + '<tr>' + '<td align="right"><a href="##" onclick="reply_an_article(' + current_article_id + ', ' + resp.rows[i].reply_id + ');">' + $("#reply").html() + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="am-icon-comment"></span>&nbsp;' + resp.rows[i].replied_times + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>' + '</tr>';
-					replies = replies + '</table></div><hr />';
-					reply_user_map.set(resp.rows[i].reply_id, resp.rows[i].user);
-				}
-				$("#article_replies_div").html(replies);
-				// 以下查询所有回复的全文
-				for (i = 0; i < len; i++) {
-					let memo        = '';
-					let next_hash   = '';
-					let content     = '';
-					let transaction = null;
-					if (resp.rows[i].storage_location === 1) {                                        // 数据存储在 EOS 链上
-						next_hash      = resp.rows[i].reply_hash;
-						do {
-							transaction = await rpc.history_get_transaction(next_hash);
-							memo = transaction.trx.trx.actions[0].data.memo;
-							next_hash = memo.slice(0, memo.indexOf('}') + 1);
-							if (next_hash.length > 2) {
-								next_hash = memo.slice(1, memo.indexOf('}'));
-							} else {
-								next_hash = '';
-							}
-							content = content + memo.slice(memo.indexOf('}') + 1, memo.length);
-						} while (next_hash != '');
-					}
-					else {      // 数据存储在其他链上
-					}
-					$(".content_of_reply_" + resp.rows[i].reply_id).html(my_escapeHTML(content));
-				}
-				// 以下查询所有目标回复的用户名
-				for (i = 0; i < len; i++) {
-					if (resp.rows[i].target_reply_id > 0) {
-						let user = '';
-						if (reply_user_map.has(resp.rows[i].target_reply_id)) {
-							user = reply_user_map.get(resp.rows[i].target_reply_id);
-							//console.log("get");
-						} else {
-							lower_bd  = new BigNumber( resp.rows[i].target_reply_id );
-							upper_bd  = new BigNumber( lower_bd.plus(1) );
-							var    r  = await rpc.get_table_rows({
-								json:  true,
-								code:  metarealnote_contract,
-								scope: metarealnote_contract,
-								table: 'replies',
-								index_position: 1,
-								key_type: 'i64',
-								lower_bound: lower_bd.toFixed(),
-								upper_bound: upper_bd.toFixed(),
-								limit: 1,
-								reverse: false,
-								show_payer: false
-							});
-							if (r.rows.length === 1) {
-								user = r.rows[0].user;
-								reply_user_map.set(resp.rows[i].target_reply_id, user);
-								//console.log("no get");
-							}
-						}
-						$(".user_of_reply_" + resp.rows[i].target_reply_id).html(my_escapeHTML(user));
-					}
-				}
 				$("#my_modal_loading").modal('close');
 				hide_all_pages();
 				$("#article_content_div").show();
@@ -373,6 +284,95 @@ function get_replies(index_position, key_type, lower_bound, upper_bound)
 	const rpc = new eosjs_jsonrpc.JsonRpc(current_endpoint);
 	(async () => {
 		try {
+			const resp = await rpc.get_table_rows({
+				json:  true,
+				code:  metarealnote_contract,
+				scope: metarealnote_contract,
+				table: 'replies',
+				index_position: index_position,
+				key_type: key_type,
+				lower_bound: lower_bound,
+				upper_bound: upper_bound,
+				limit: items_per_page,
+				reverse: false,
+				show_payer: false
+			});
+			let replies = '';
+			let i       = 0;
+			let len     = resp.rows.length;
+			if (len === 0) {
+				replies = '&nbsp;';
+			}
+			// 以下生成所有回复的基本信息
+			for (i = 0; i < len; i++) {
+				let f = '<a href="##" onclick="alert(\'' + $("#head_hash").html() + storage_locations[resp.rows[i].storage_location] + '{' + resp.rows[i].reply_hash + '}\');">id' + resp.rows[i].reply_id + '</a>&nbsp;&nbsp;';
+				if (resp.rows[i].target_reply_id > 0) {
+					f = f + $("#reply_to").html() + '&nbsp;<span class="user_of_reply_' + resp.rows[i].target_reply_id + '">&nbsp;</span>&nbsp;id' + resp.rows[i].target_reply_id;
+				}
+				replies = replies + '<div><table width="100%" border="0">';
+				replies = replies + '<tr>' + '<td rowspan="3" width="30" align="center" valign="top">&nbsp;</td>' + '<td rowspan="3" width="64" align="center" valign="top"><span class="am-icon-user"></span></td>' + '<td>' + resp.rows[i].user + '&nbsp;&nbsp;' + timestamp_trans_full(resp.rows[i].post_time) + '</td>' + '</tr>';
+				replies = replies + '<tr>' + '<td>' + f + '<pre class="content_of_reply_' + resp.rows[i].reply_id + '">&nbsp;</pre></td>' + '</tr>';
+				replies = replies + '<tr>' + '<td align="right"><a href="##" onclick="reply_an_article(' + current_article_id + ', ' + resp.rows[i].reply_id + ');">' + $("#reply").html() + '</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="am-icon-comment"></span>&nbsp;' + resp.rows[i].replied_times + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>' + '</tr>';
+				replies = replies + '</table></div><hr />';
+				reply_user_map.set(resp.rows[i].reply_id, resp.rows[i].user);
+			}
+			$("#article_replies_div").html(replies);
+			// 以下查询所有回复的全文
+			for (i = 0; i < len; i++) {
+				let memo        = '';
+				let next_hash   = '';
+				let content     = '';
+				let transaction = null;
+				if (resp.rows[i].storage_location === 1) {                                        // 数据存储在 EOS 链上
+					next_hash      = resp.rows[i].reply_hash;
+					do {
+						transaction = await rpc.history_get_transaction(next_hash);
+						memo = transaction.trx.trx.actions[0].data.memo;
+						next_hash = memo.slice(0, memo.indexOf('}') + 1);
+						if (next_hash.length > 2) {
+							next_hash = memo.slice(1, memo.indexOf('}'));
+						} else {
+							next_hash = '';
+						}
+						content = content + memo.slice(memo.indexOf('}') + 1, memo.length);
+					} while (next_hash != '');
+				}
+				else {      // 数据存储在其他链上
+				}
+				$(".content_of_reply_" + resp.rows[i].reply_id).html(my_escapeHTML(content));
+			}
+			// 以下查询所有目标回复的用户名
+			for (i = 0; i < len; i++) {
+				if (resp.rows[i].target_reply_id > 0) {
+					let user = '';
+					if (reply_user_map.has(resp.rows[i].target_reply_id)) {
+						user = reply_user_map.get(resp.rows[i].target_reply_id);
+						//console.log("get");
+					} else {
+						lower_bd  = new BigNumber( resp.rows[i].target_reply_id );
+						upper_bd  = new BigNumber( lower_bd.plus(1) );
+						var    r  = await rpc.get_table_rows({
+							json:  true,
+							code:  metarealnote_contract,
+							scope: metarealnote_contract,
+							table: 'replies',
+							index_position: 1,
+							key_type: 'i64',
+							lower_bound: lower_bd.toFixed(),
+							upper_bound: upper_bd.toFixed(),
+							limit: 1,
+							reverse: false,
+							show_payer: false
+						});
+						if (r.rows.length === 1) {
+							user = r.rows[0].user;
+							reply_user_map.set(resp.rows[i].target_reply_id, user);
+							//console.log("no get");
+						}
+					}
+					$(".user_of_reply_" + resp.rows[i].target_reply_id).html(my_escapeHTML(user));
+				}
+			}
 			$("#my_modal_loading").modal('close');
 		} catch (e) {
 			$("#my_modal_loading").modal('close');
