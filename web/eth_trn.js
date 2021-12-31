4e892a77873b47e778ffc139a9e4c7fd98df17a4
niuyg <y-g.niu@163.com>
2021-12-31 14:51:14 +0000

    edit web

--- web/eth_trn.js
+++ web/eth_trn.js
@@ -1,3 +1,26 @@
 
 // utf8编码
 
+function check_eth_metamask_installed()
+{
+	if (typeof window.ethereum !== 'undefined') {
+		return true;
+	} else {
+		alert( $("#metamask_not_installed").html() );
+		return false;
+	}
+}
+
+function check_eth_metamask_connected()
+{
+	if ( eth_user_account !== '' && window.ethereum.isConnected() ) {
+		return true;
+	} else {
+		alert( $("#metamask_not_connected").html() );
+		return false;
+	}
+}
+
+function eth_connect_metamask()
+{
+}
