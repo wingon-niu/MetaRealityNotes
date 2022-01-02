f7468bfa5326120e474f2861899fa808caafedb7
niuyg <y-g.niu@163.com>
2022-01-02 16:17:18 +0000

    edit web

--- web/eth_postarticle.js
+++ web/eth_postarticle.js
@@ -3,4 +3,12 @@
 
 function eth_do_post_article(my_category, my_type, my_storage_location, my_forward_article_id, my_quantity, strArray)
 {
+	(async () => {
+		try {
+			//
+		} catch (e) {
+			if (typeof e === 'object') alert(e.message);
+			else                       alert(e);
+		}
+	})();
 }
