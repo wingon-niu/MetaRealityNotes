b140db248db7e5b76cab48754dfc2ad1665f0d3e
niuyg <y-g.niu@163.com>
2022-01-12 10:52:58 +0000

    edit web

--- web/eth_postarticle.js
+++ web/eth_postarticle.js
@@ -26,6 +26,39 @@ function eth_do_post_article(my_category, my_type, my_storage_location, my_forwa
 				trn_hash    = result;
 			}
 			post_article_current_index = -1;
+			if (post_article_write_to_table === false) {
+				let account = scatter.identity.accounts.find(x => x.chainId === current_network.chainId);
+				const rpc = new eosjs_jsonrpc.JsonRpc(current_endpoint);
+				const api = scatter.eos(current_network, eosjs_api.Api, {rpc, textDecoder: new TextDecoder(), textEncoder: new TextEncoder(), beta3:true});
+				let eos_result = await api.transact(
+					{
+						actions: [{
+							account: metarealnote_contract,
+							name: 'postarticle',
+							authorization: [{
+								actor: account.name,
+								permission: account.authority
+							}],
+							data: {
+								user:               account.name,
+								article_hash:       trn_hash,
+								num_of_trns:        strArray.length,
+								category:           my_category,
+								type:               my_type,
+								storage_location:   my_storage_location,
+								forward_article_id: my_forward_article_id
+							}
+						}]
+					},{
+						blocksBehind: 3,
+						expireSeconds: 60
+					}
+				);
+				if (typeof(eos_result) === 'object' && eos_result.transaction_id != "") {
+					trn_success                 = true;
+					post_article_write_to_table = true;
+				} else { trn_failed(); return; }
+			}
 			alert("OK");
 		} catch (e) {
 			trn_success = false;
