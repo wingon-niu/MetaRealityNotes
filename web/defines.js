c3e28c9cc4be71076a34ad291c9e7c475664eec0
niuyg <y-g.niu@163.com>
2022-01-23 04:25:33 +0000

    edit web

--- web/defines.js
+++ web/defines.js
@@ -94,3 +94,4 @@ if (runmode === "prod") {    // 生产环境
 
 const eth_article_preview_length = 100;    // 存放在ETH链上的文章在文章列表中显示的预览长度
 
+const articles_sort_by = "article_post_time";    // 文章在文章列表中的排序方式：article_post_time/last_replied_time
