11e108baac6bdbd8cc3c7c1db120248b1ed7094b
niuyg <y-g.niu@163.com>
2022-05-03 09:31:28 +0000

    edit web

--- web/defines.js
+++ web/defines.js
@@ -56,12 +56,13 @@ var post_reply_current_index    = -1;
 
 const eos_per_trn_len     = 63;           // 一个utf8编码的汉字3个字节，63*3+64+2=255，刚好可以放进eos交易的memo中
 const eth_per_trn_len     = 3336;         // 一个utf8编码的汉字3个字节，每个交易的备注长度为：3336*3+64+2+2=10076字节，需要使用的gas数量为：10076*68+21000=706168个
+const arweave_per_trn_len = 10;           // 一个utf8编码的汉字3个字节，100000汉字*3=300000字节，300000字节/1024=292.96875 KB
 const btc_per_trn_len     = 1000;
 
 var current_page   = "home"; // 当前页面：home/my_articles/my_replies/users_i_follow/users_follow_me/articles_of_user_i_follow/articles_of_user_follow_me
 var articles_array = [0];    // 数组，保存所有打开的文章的ID，因为从文章中可以打开别的文章，所以可能会有多个打开的文章，用于点击后退按钮功能时从一个文章后退到前一个文章，逐个进行后退
 
-const storage_locations = ['WHO', 'EOS', 'ETH', 'BSC', 'HECO', 'BTC'];
+const storage_locations = ['WHO', 'EOS', 'ETH', 'BSC', 'HECO', 'BTC', 'Arweave'];
 const items_per_page    = 10; // 每页显示条数
 
 var   preview_of_article_map = null;
@@ -92,7 +93,8 @@ if (runmode === "prod") {    // 生产环境
 	eth_gasLimit      = '0x107ac0';     // 1080000个gas
 }
 
-const eth_article_preview_length = 100;    // 存放在ETH链上的文章在文章列表中显示的预览长度
+const eth_article_preview_length     = 100;      // 存放在ETH链上的文章在文章列表中显示的预览长度
+const arweave_article_preview_length = 100;      // 存放在Arweave链上的文章在文章列表中显示的预览长度
 
 const articles_sort_by = "last_replied_time";    // 文章在文章列表中的排序方式： article_post_time/last_replied_time
 const replies_sort_by  = "ascending_order";      // 回复在回复列表中的排序方式： ascending_order/descending_order
@@ -114,5 +116,5 @@ if (runmode === "prod") {    // 生产环境
 	amount_per_trn_reply_conf_eth   = '0.000000000000000001 ETH';
 }
 
-const storage_locations_supported_conf = 'ETH';      //   'EOS, ETH, BSC, HECO, BTC'
+const storage_locations_supported_conf = 'ETH, Arweave';      //   'EOS, ETH, BSC, HECO, BTC, Arweave'
 
