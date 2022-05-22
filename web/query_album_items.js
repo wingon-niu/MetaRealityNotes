e056a4eafbba7f94d76906ea46b3d7aa76b02718
niuyg <y-g.niu@163.com>
2022-05-22 13:33:08 +0000

    edit web

--- web/query_album_items.js
+++ web/query_album_items.js
@@ -34,4 +34,111 @@ function get_home_page_album_items()
 
 function get_album_items(index_position, key_type, lower_bound, upper_bound)
 {
+	$("#my_modal_loading").modal('open');
+	const rpc = new eosjs_jsonrpc.JsonRpc(current_endpoint);
+	(async () => {
+		try {
+			const resp = await rpc.get_table_rows({
+				json:  true,
+				code:  metarealnote_contract,
+				scope: metarealnote_contract,
+				table: 'albums',
+				index_position: index_position,
+				key_type: key_type,
+				lower_bound: lower_bound,
+				upper_bound: upper_bound,
+				limit: album_items_per_page,
+				reverse: false,
+				show_payer: false
+			});
+			let album_items = '';
+			let i           = 0;
+			let len         = resp.rows.length;
+			if (len === 0) {
+				if (get_cookie('i18n_lang') === "zh") album_items = '<div>查询结果为空。</div>';
+				else                                  album_items = '<div>The query result is empty.</div>';
+			}
+			// 以下生成所有条目的基本信息
+			for (i = 0; i < len; i++) {
+				album_items = album_items + '<div class="am-u-sm-6 am-u-md-4 am-u-lg-2">';
+				album_items = album_items + '<div id="album_item_div_' + resp.rows[i].item_id + '" style="width:168px; height:168px;">';
+				if (resp.rows[i].item_type === 1) {                                       // 图片
+					album_items = album_items + '<img id="album_item_img_' + resp.rows[i].item_id + '" src="" alt="loading..." style="width:auto; height:auto; max-width:100%; max-height:100%;" />';
+				}
+				else if (resp.rows[i].item_type === 2) {                                  // 视频
+					album_items = album_items + 'Video';
+				}
+				else if (resp.rows[i].item_type === 3) {                                  // 音频
+					album_items = album_items + 'Audio';
+				}
+				else if (resp.rows[i].item_type === 5) {                                  // 其他
+					album_items = album_items + 'Other';
+				}
+				else {
+					album_items = album_items + '';
+				}
+				album_items = album_items + '</div>';
+				album_items = album_items + '<div><a href="##" onclick="alert(\'' + $("#head_hash").html() + storage_locations[resp.rows[i].storage_location] + '{' + resp.rows[i].origin_head_hash + '}\');">id' + resp.rows[i].item_id + '</a>&nbsp;</div>';
+				album_items = album_items + '<div>' + timestamp_trans_full(resp.rows[i].post_time) + '</div>';
+				album_items = album_items + '<div id="album_item_file_name_' + resp.rows[i].item_id + '"></div>';
+				album_items = album_items + '<div><pre>' + my_escapeHTML(resp.rows[i].description) + '</pre></div>';
+				album_items = album_items + '<div>此处放操作下拉框</div>';
+				album_items = album_items + '</div>';
+			}
+			// 下一页
+			if (resp.more === true) {
+				album_items = album_items + '<table width="100%" border="0"><tr><td align="center"><a href="##" onclick="get_album_items(' + index_position + ', \'' + key_type + '\', \'' + resp.next_key + '\', \'' + upper_bound + '\');">' + $("#next_page").html() + '</a></td></tr></table>';
+			}
+			// 显示
+			$("#album_items_div").html(album_items);
+			// 以下查询所有图片的内容
+			for (i = 0; i < len; i++) {
+				//if (content_of_reply_map.has(resp.rows[i].reply_id)) {
+				//	$(".content_of_reply_" + resp.rows[i].reply_id).html(my_escapeHTML(content_of_reply_map.get(resp.rows[i].reply_id)));
+				//	//console.log("get");
+				//} else {
+				//	let memo        = '';
+				//	let next_hash   = '';
+				//	let content     = '';
+				//	let transaction = null;
+				//	if (resp.rows[i].storage_location === 1) {                                        // 数据存储在 EOS 链上
+				//	}
+				//	else if (resp.rows[i].storage_location === 2) {                                   // 数据存储在 ETH 链上
+				//	}
+				//	else if (resp.rows[i].storage_location === 6) {                                   // 数据存储在 Arweave 链上
+				//		try {
+				//			next_hash   = resp.rows[i].reply_hash;
+				//			do {
+				//				let ar_response = await fetch(arweave_endpoint + next_hash);
+				//				memo = await ar_response.text();
+				//				next_hash = memo.slice(0, memo.indexOf('}') + 1);
+				//				if (next_hash.length > 2) {
+				//					next_hash = memo.slice(1, memo.indexOf('}'));
+				//				} else {
+				//					next_hash = '';
+				//				}
+				//				content = content + memo.slice(memo.indexOf('}') + 1, memo.length);
+				//			} while (next_hash != '');
+				//		}
+				//		catch (e) {                                  // 找不到某个交易hash对应的交易，可能是这个交易没有被打包进区块，被丢弃了。
+				//			content = content + $("#content_chain_interruption_info_1").html() + next_hash + $("#content_chain_interruption_info_2").html();
+				//		}
+				//	}
+				//	else {      // 数据存储在其他链上
+				//	}
+				//	if (resp.rows[i].content_sha3_hash !== Web3.utils.sha3(content).slice(2)) {      // 内容的Sha3 Hash不一致
+				//		if (get_cookie('i18n_lang') === "zh") alert("错误：回复的内容的Sha3 Hash不一致。回复id=" + resp.rows[i].reply_id + "。");
+				//		else                                  alert("Error: The sha3 hash of content of the reply is not matched. Reply id=" + resp.rows[i].reply_id + ".");
+				//	}
+				//	$(".content_of_reply_" + resp.rows[i].reply_id).html(my_escapeHTML(content));
+				//	content_of_reply_map.set(resp.rows[i].reply_id, content);
+				//	//console.log("no get");
+				//}
+			}
+			$("#my_modal_loading").modal('close');
+		} catch (e) {
+			$("#my_modal_loading").modal('close');
+			alert(e);
+		}
+	})();
 }
