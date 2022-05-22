75c00646ceeed5967f5ad617f5ec2ed914951c5d
niuyg <y-g.niu@163.com>
2022-05-22 10:58:18 +0000

    edit web

--- web/query_album_items.js
+++ web/query_album_items.js
@@ -3,4 +3,9 @@
 
 function get_home_page_album_items()
 {
+	if(current_user_account === "") {
+		if (get_cookie('i18n_lang') === "zh") $("#album_items_div").html("请登录EOS账户之后查看。");
+		else                                  $("#album_items_div").html("Please log in to the EOS account.");
+		return;
+	}
 }
